a649dc56f4f5b8b47d1053286d24b4ff3de3661a
nabhatra <nabhatra@yahoo.com>
2023-07-23 18:56:30 +0700

    removed expanded from tab widget to fixed error

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -21,39 +21,38 @@ class _DashboardState extends State<Dashboard> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Expanded(
-          child: TabBarView(
+      body: Column(
+        children: [
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                //import menu_page.dart
+                const MenuPage(),
+                Container(color: Colors.amber),
+                Container(color: Colors.blue),
+                Container(color: Colors.red),
+              ],
+            ),
+          ),
+          Container(
+            height: 20,
+          ),
+          TabBar(
             controller: _tabController,
-            children: [
-              //import menu_page.dart
-              const MenuPage(),
-              Container(color: Colors.amber),
-              Container(color: Colors.blue),
-              Container(color: Colors.red),
-            ],
-          ),
-        ),
-        Container(
-          height: 20,
-        ),
-        TabBar(
-          controller: _tabController,
-          indicator: BoxDecoration(
-            gradient: Constant.linearBackgroundColor,
-            borderRadius: BorderRadius.circular(40),
-          ),
-          indicatorColor: Colors.white,
-          labelColor: Colors.white,
-          indicatorSize: TabBarIndicatorSize.tab,
-          indicatorWeight: 0,
-          dividerColor: Colors.transparent,
-          splashFactory: NoSplash.splashFactory,
-          padding: EdgeInsets.only(left: 10, bottom: 20, right: 10),
-          tabs: [
-            Expanded(
-              child: Tab(
+            indicator: BoxDecoration(
+              gradient: Constant.linearBackgroundColor,
+              borderRadius: BorderRadius.circular(40),
+            ),
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorWeight: 0,
+            dividerColor: Colors.transparent,
+            splashFactory: NoSplash.splashFactory,
+            padding: EdgeInsets.only(left: 10, bottom: 20, right: 10),
+            tabs: [
+              Tab(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
@@ -61,50 +60,50 @@ class _DashboardState extends State<Dashboard> with TickerProviderStateMixin {
                     Text(
                       "Home",
                       style: TextStyle(fontSize: 11),
-                    )
+                    ),
                   ],
                 ),
               ),
-            ),
-            Tab(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.shop),
-                  Text(
-                    "Product",
-                    style: TextStyle(fontSize: 11),
-                  )
-                ],
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.shop),
+                    Text(
+                      "Product",
+                      style: TextStyle(fontSize: 11),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Tab(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.history),
-                  Text(
-                    "History",
-                    style: TextStyle(fontSize: 11),
-                  )
-                ],
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.history),
+                    Text(
+                      "History",
+                      style: TextStyle(fontSize: 11),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Tab(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.portrait_rounded),
-                  Text(
-                    "Profile",
-                    style: TextStyle(fontSize: 11),
-                  )
-                ],
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.portrait_rounded),
+                    Text(
+                      "Profile",
+                      style: TextStyle(fontSize: 11),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
-      ],
-    ));
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
